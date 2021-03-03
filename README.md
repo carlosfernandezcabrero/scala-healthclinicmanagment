56bb618c08dd155a7d1fad9bdd90f57e66415eed
carlosfernandezcabrero <carlitostrabajo2018@outlook.es>
2021-03-03 18:32:13 +0100

    Fix issue when dni of patient is only letters

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # scala-healthclinicmanagment
+<hr>
+<p>Application for learning the Scala language.</p>
+<p>Technical data:</p>
+<ul>
+    <li>Scala version: 2.13.5</li>
+    <li>Java version: 1.8</li>
+</ul>
--- src/main/scala/model/Person.scala
+++ src/main/scala/model/Person.scala
@@ -12,17 +12,18 @@ abstract class Person (
 
   /**
    * Method that checks if the dni is valid
+   * @throws InvalidDniException When the dni isn't correct
    */
   def checkDni(): Unit = {
-    val letIntro = dni.charAt(dni.length - 1).toUpper
-    val numIntro = dni.substring(0, dni.length() - 1).toInt
     val pat = Pattern.compile("[0-9]{7,8}[A-Za-z]")
     val mat = pat.matcher(dni)
-    val alphabet = List('T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H',
-      'L', 'C', 'K', 'E')
+    //val alphabet = List('T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H',
+    //    'L', 'C', 'K', 'E')
+    //val letIntro = dni.charAt(dni.length - 1).toUpper
+    //val numIntro = dni.substring(0, dni.length() - 1).toInt
 
     // Strict mode: if ((!mat.matches()) || (mat.matches() && letIntro != alphabet(numIntro % 23))){
-    if (!mat.matches()){
+    if (!mat.matches()) {
       throw new InvalidDniException("ERROR: The dni is invalid")
     }
   }
